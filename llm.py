966e9b90fea6589a71153b0c38206ea251edac2a
wxj_windows <wxj122350@126.com>
2026-06-02 22:24:46 +0800

    <fix>部分程序修改

--- llm.py
+++ llm.py
@@ -1,8 +1,15 @@
 from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
+from langchain_ollama.chat_models import ChatOllama
+
 
 bailian_llm = ChatOpenAI(
     model = "qwen3-max",
     base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1",
     api_key = SecretStr("sk-0447fbe1a6fb43d38cb83123bf7a0fd0"),
+)
+
+ollama_llm = ChatOllama(
+    model = "qwen3.5:0.8b",
+    temperature = 0.2
 )
--- mcp_learning/mcp_client.py
+++ mcp_learning/mcp_client.py
@@ -6,6 +6,7 @@ from langchain.agents import create_agent
 print(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import llm
+from langchain_core.prompts import PromptTemplate
 
 async def create_mcp_client():
     mcp_key = os.environ.get("AMAP_KEY")
@@ -25,45 +26,19 @@ async def create_mcp_client():
 
 async def create_and_run_mapAgent():
     client, tools = await create_mcp_client()
-    # llm = ChatOllama(
-    #     model = "qwen3.5:0.8b",
-    #     # tools = tools,
-    #     # system_prompt = "你是一个具有高德导航工具箱的导航智能体"
-    # )
 
     agent = create_agent(
-        model = llm.bailian_llm,
+        model = llm.ollama_llm,
         tools = tools,
-        system_prompt = "你是一个具有高德导航工具箱的导航智能体",
+        # system_prompt = "你是一个具有高德导航工具箱的导航智能体",
     )
 
-    while True:
-        user_input = input("🔹 你想让 Agent 做什么？\n> ")
-        resp = await agent.ainvoke(
-            {"messages" : [{"role" : "user", "content" : user_input}]}
-        )
+    prompt_temp = PromptTemplate.from_template("你是一个导航智能助手，可以调用高德的mcp工具\n\n问题: {input}")
+    prompt = prompt_temp.format(input = "帮我规划一条从成都清园小区到古蔺白沙小学的路线")
 
-        print(resp)
+    resp = await agent.ainvoke(prompt)
 
-        # for key, values in resp.items():
-        #     print("key:\n{}".format(key))
-        #     print("values:")
-        #     for value in values:
-        #         print(value)
-        #     print()
-
-        # print("-------------------------------------")
-        # print(resp["messages"][-1].content)
-        
-        # continue_ques = input("是否继续对话(y or n)?:\n")
-        # while continue_ques not in ["y", "n"]:
-        #     continue_ques = input("你猪脑子吗?看不懂人话?滚回去重新输一遍，再错把你骨灰扬了！！！\n")
-        #     if continue_ques not in ["y", "n"]:
-        #         print("说不明白了！算了，我妈不让我跟傻子玩儿")
-        #         break_conversation = True
-        #         break
-        # if continue_ques == "n" or break_conversation:
-        #     break
+    print(resp)
 
 
 if __name__ == "__main__":
